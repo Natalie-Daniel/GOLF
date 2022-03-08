431e7e058c44d7710e5e7db047c603e0391ad928
GatorDJ98 <97544775+GatorDJ98@users.noreply.github.com>
2022-03-08 11:10:48 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,9 @@
-$(document).ready(function () {
-  console.log("Doc's Ready");
+const info = ["dodle@una.edu", "CIS-376", "University of North Alabama"];
 
 
 
-  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+for (j = 0; j < info.length; j++) {
+    //do something
+    $("#arrayOutput").append(info[j] + "<br/>");
   }
+  
