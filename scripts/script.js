3e957d45960b44afa88e392b5126eb84af476184
GatorDJ98 <97544775+GatorDJ98@users.noreply.github.com>
2022-03-08 11:51:10 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
+const classCharlies = [
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+    "link",
+];
+
+$.each(classCharlies, function(index, value) {
+    $("body").append(
+        '  <div class="mx-auto w-25 card text-center bg-primary"> <div class="card-header">Game No. ' +
+        index +
+        '</div>   <div class="card-body">    <a href="' +
+        value +
+        '">' +
+        value +
+        "</a>  </div></div> "
+    );
+});
+
 const info = ["dodle@una.edu", "CIS-376", "University of North Alabama"];
 
 
@@ -5,5 +38,4 @@ const info = ["dodle@una.edu", "CIS-376", "University of North Alabama"];
 for (j = 0; j < info.length; j++) {
     //do something
     $("#arrayOutput").append(info[j] + "<br/>");
-  }
-  
+}
