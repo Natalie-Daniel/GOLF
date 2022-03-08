3e1d86b7545155bb41088f556de0e57a39d607c7
GatorDJ98 <97544775+GatorDJ98@users.noreply.github.com>
2022-03-08 11:54:56 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -30,12 +30,3 @@ $.each(classCharlies, function(index, value) {
         "</a>  </div></div> "
     );
 });
-
-const info = ["dodle@una.edu", "CIS-376", "University of North Alabama"];
-
-
-
-for (j = 0; j < info.length; j++) {
-    //do something
-    $("#arrayOutput").append(info[j] + "<br/>");
-}
