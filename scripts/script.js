d735e2f9eeabce4f629fcd145cbce1db8791044c
GatorDJ98 <97544775+GatorDJ98@users.noreply.github.com>
2022-03-10 11:24:28 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,15 @@
 const classCharlies = [
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
-    "link",
+    "https://almostheaven52.github.io/Professor-Doge-2.0/",
+    "https://codepen.io/cdman814/full/qBPJGzK",
+    "https://peytonjunjulas.github.io/labCharlie/",
+    "https://frosty9424.github.io/frostgames/",
+    "https://kenrick95.github.io/c4/",
+    "https://laurenmalone01.github.io/echo-lab/",
+    "https://errorless-stone.github.io/edgy-bourbon/",
+    "https://htmlgames.github.io/htmlgames/differences/jump/index.html",
+    "https://kobyingram11.github.io/scaling-palm-tree/",
+    "https://sleep-calculator.vercel.app/",
+
 ];
 
 $.each(classCharlies, function(index, value) {
